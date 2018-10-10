645aac44ee1a5eb5e23864428c11a76497df5df0
ymzk-jp <ymzk@gmail.com>
2018-10-10 23:09:02 +0900

    微調整

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const Lottery = require('./lottery');
 let v = new Lottery();
 
-for(let i=0; i<5;i++){
+for (let i = 0; i < 5; i++) {
     const voter_id = v.voterId();
     const product_id = v.productId();
-    for(let j=0;j<i;j++){
+    for (let j = 0; j < i; j++) {
         const lottery_num = v.lotteryNum();
-        v.bark(voter_id,product_id,lottery_num);
+        v.bark(voter_id, product_id, lottery_num);
     }
 }
